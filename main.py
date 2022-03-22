2ee219b0534224652c3e9f1b8f1e9f2531c6617b
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2022-03-22 10:51:55 +0100

    Case pressed

--- main.py
+++ main.py
@@ -10,9 +10,9 @@
 #  IMPORTS  #
 #############
 import random
-
-import pygame
 import sys
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
 
 
 #############
@@ -28,11 +28,13 @@ class Grid:
     def start(self, x: int, y: int):
         rnd_grid = [[random.random() for _ in range(self.size)] for _ in range(self.size)]
 
+        # éviter de tomber direct sur une bombe et d'avoir des bombes juste a coté
         for i in range(3):
             for j in range(3):
                 if self.size > y+i and 0 <= y+i-1 and self.size > x+j and 0 <= x+j-1:
                     rnd_grid[y+(i-1)][x+(j-1)] = 0
 
+        # Récupère les self.bombes plus grandes valeurs pour les mettre dans la grille 
         for bombe in range(self.bombes):
             m = 0
             coords = ()
@@ -51,6 +53,36 @@ class Grid:
                 if not case.is_discovered:
                     b = False
         return self.finished or b
+    
+    def count_nb_de_voisins_bombes(self, x, y):
+        voisins_bombes = 0
+        for i in range(3):
+            for j in range(3):
+                if self.size > y+i and 0 <= y+i-1 and self.size > x+j and 0 <= x+j-1:
+                     voisins_bombes += self.grid[y+(i-1)][x+(j-1)].is_bombe
+        return voisins_bombes
+
+    def case_press(self, x, y):
+        if self.grid[x][y].is_discovered:
+            print("Case déjà découverte, Rejouez")
+            return
+        if self.grid[x][y].is_bombe:
+            self.finished = True
+            print("Vous avez perdu")
+            return
+        nb_de_bombdes_autour = self.count_nb_de_voisins_bombes(x, y)
+        if nb_de_bombdes_autour == 0:
+            for i in range(3):
+                for j in range(3):
+                    if self.size > y+i and 0 <= y+i-1 and self.size > x+j and 0 <= x+j-1:
+                        self.case_press(y+(i-1), x+(j-1))
+        
+        self.grid[x][y].nb_bombes = nb_de_bombdes_autour
+        
+
+
+
+
 
 
 class Case:
@@ -61,10 +93,12 @@ class Case:
         self.sprite = None
 
 
+
 class GLOBALS:
     grid_size = 10
     bombes = 5
     grid = Grid(grid_size, bombes)
+    debug = False
 
 
 #############
@@ -72,6 +106,11 @@ class GLOBALS:
 #############
 def main():
     GLOBALS.grid.start(0, 0)
+    GLOBALS.grid.case_press(5, 5)
+    if debug:
+        print(GLOBALS.bombes)
+        print(GLOBALS.grid_size)
+        print(GLOBALS.debug)
 
 
 #############
@@ -79,7 +118,16 @@ def main():
 #############
 if __name__ == "__main__":
     argv = sys.argv[1:]  # Eviter le python et nom du programe
-    if 1 <= len(argv) <= 2:
+    if '--help' in argv:
+        print("[-] Usage: python main.py [--help] [--debug] [[grid_size] [nb_de_bombdes]]")
+        exit(0)
+    if '--debug' in argv:
+        del argv[argv.index("--debug")]
+        GLOBALS.debug = True
+    if 2 <= len(argv):
         GLOBALS.grid_size = argv[0]
-        GLOBALS.nb_de_debombes = argv[1]
+        GLOBALS.bombes = argv[1]
+    elif 1 <= len(argv):
+        print("[-] Usage: python main.py [--help] [--debug] [[grid_size] [nb_de_bombdes]]")
+        print(f"\t\\Default: Size={GLOBALS.grid_size}, bombes={GLOBALS.bombes}")
     main()
